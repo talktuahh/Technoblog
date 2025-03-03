4b51aa091d92ca571bc7c8104c4f579464b29361
talktuahh <bryan.robuschi@stud.krw.ch>
2025-03-04 00:55:05 +0100

    Featurereiche SQL Database eingebaut für Artikeln

--- main.php
+++ main.php
@@ -1,3 +1,9 @@
+<?php
+    include "db.php";
+    $sql = "SELECT * FROM articles";
+    $result = $conn->query($sql);
+?>
+
 <html>
     <head>
         <title>Technoblog - Home</title>
@@ -5,5 +11,18 @@
     </head>
     <body>
         <h1><b>Main Page</b></h1>
+        <ul>
+        <?php
+        if ($result && $result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo '<a href="article.php?id=' . $row['article_id'] . '">';
+                echo '<button>' . htmlspecialchars($row['article_title']) . '</button>';
+                echo '</a><br>';
+            }
+        } else {
+            echo "<p>Keine Artikel gefunden.</p>";
+        }
+        ?>
+    </ul>
     </body>
 </html>
